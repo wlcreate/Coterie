eb5f4d7867c77032efce6a5ca66a06b6a559379e
wlcreate <waverley.nyc@gmail.com>
2020-09-04 19:34:17 -0400

    fixes BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :meetings #this is the host
     has_many :registrations
     has_many :meetings, through: :registrations #this is the attendee
+
+    has_secure_password
 end
